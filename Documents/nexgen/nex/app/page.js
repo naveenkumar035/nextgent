73a8553a9e0696249fd7ff131832b779127356e8
naveenkumar035 <gowthamangowthaman1276@gmail.com>
2024-07-10 17:18:47 +0530

    z

--- Documents/nexgen/nex/app/page.js
+++ Documents/nexgen/nex/app/page.js
@@ -4,11 +4,16 @@ import About from "./components/About";
 import Sports from "./components/Sports";
 import Activities from "./components/Activities";
 import Programs from "./components/Programs";
+import Head from "next/head"
 
 
  function Home() {
   return (
     <>
+     <Head>
+        <title>NextGen</title>
+        <meta name="description" content="As we look to the future, we aim to expand our reach and continue our tradition of excellence in tennis coaching. We are committed to discovering and nurturing the next generation of tennis champions, providing them with the resources and training they need to succeed on both national and international stages." />
+        </Head>
     <div className=" flex flex-col h-screen bg-white snap-y snap-mandatory overflow-scroll z-0  " >   
     
       <section id="hero" className="snap-start" >
